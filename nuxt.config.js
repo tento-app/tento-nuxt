9a8ee0aabab2fa0f53feeb48ed17e8423a5dcbd3
ergofriend <altsir.cgc.yhyavr@gmail.com>
2019-02-07 21:43:29 +0900

    camp and user page

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,53 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Tento',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: '新たな学生コミュニティ' },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Tento'
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://tento.app'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Tento'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: '新たな学生コミュニティ'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/logo.png'
+      },
+      // Twitter
+      {
+        name: 'twitter:card',
+        content: 'summary'
+      },
+      {
+        name: 'twitter:site',
+        content: '@tento_app'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 

a211990be4d167bde8e1bacfc482a193c0839f11
Kensei Yaegashi <s1F101702857@iniad.org>
2019-03-05 12:38:51 +0900

    'アカウント詳細'

---
+++ components/account-edit.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <div class="account_form">
+    <form class="" action="index.html" method="post">
+      <div class="account_form_icon">
+        <input type="file" name="" value="">
+        <p>アイコン変更</p>
+      </div>
+      <div class="account_form_text">
+        <div class="account_form_text_name">
+          <input type="text" name="" value="">
+        </div>
+
+        <div class="account_form_text_position">
+          <input type="text" name="" value="">
+        </div>
+
+        <div class="account_form_text_intro">
+          <textarea name="name" rows="8" cols="80"></textarea>
+        </div>
+        <div class="">
+          <button type="button" name="button" class="btn">この内容を保存</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/style/_color.scss';
+
+.account_form{
+  background-color: #fff;
+  box-shadow: $box_shadow02;
+  border-radius: 12px;
+  padding: 1.5rem;
+  margin: 80px auto;
+  max-width: 600px;
+  form{
+    display: flex;
+    input{
+      background-color: #fafafa;
+      border: #cacaca solid 1px
+      border-radius: 4px;
+      font-size: 14px;
+      padding: 0.4rem;;
+    }
+  }
+}
+</style>
---
+++ pages/account-edit.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <section id="account">
+    <Header />
+    <div class="main">
+      <div class="account_edit">
+        <AccountEdit />
+      </div>
+    </div>
+    <Footer />
+  </section>
+</template>
+
+<script>
+import AccountEdit from '~/components/account-edit.vue';
+import Header from '~/layouts/Header.vue';
+import Footer from '~/layouts/Footer.vue';
+
+export default {
+  components: {
+    AccountEdit,
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/style/base.scss';
+@import '~/assets/style/btn.scss';
+
+.main{
+  .account_header{
+
+  }
+  .skill_container{
+    padding: 80px 0;
+    margin-top: 80px;
+    background-color: #fafafa;
+  }
+}
+
+</style>

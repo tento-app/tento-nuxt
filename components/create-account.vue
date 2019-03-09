f1e013ea90dfca5f493d38d4b1ebfdc808d91523
Kensei Yaegashi <s1F101702857@iniad.org>
2019-03-10 00:30:30 +0900

    'login'

---
+++ components/create-account.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+  <div class="">
+    <div class="create-account">
+      <div class="sign-in">
+        <h1>Let's join new tento</h1>
+        <div class="img">
+
+        </div>
+        <form class="" action="index.html" method="post">
+          <div class="">
+            <label for="in-e-mail">Email</label>
+            <input type="text" name="" value="" id="in-e-mail">
+          </div>
+          <div class="">
+            <label for="in-password">password</label>
+            <input type="password" name="" value="" id="in-password">
+          </div>
+        </form>
+      </div>
+
+      <div class="sign-up">
+        <h1>Create new account</h1>
+        <div class="">
+          <label for="up-e-mail">Email</label>
+          <input type="text" name="" value="" id="up-e-mail">
+        </div>
+        <div class="">
+          <label for="name">User name</label>
+          <input type="text" name="" value="" id="name">
+        </div>
+        <div class="">
+          <label for="up-password">password</label>
+          <input type="password" name="" value="" id="up-password">
+        </div>
+        <div class="">
+          <label for="conf-up-password">password(confirm)</label>
+          <input type="password" name="" value="" id="conf-up-password">
+        </div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/style/base.scss';
+@import '~/assets/style/btn.scss';
+
+.create-account{
+  display: flex;
+  label {
+    display: block;
+    font-weight: bold;
+    font-size: 14px;
+    color: #1f1f1f;
+  }
+}
+
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <section id="account">
+    <Header />
+    <div class="main">
+      <createAccount />
+    </div>
+  </section>
+</template>
+
+<script>
+import createAccount from '~/components/create-account.vue';
+import Header from '~/layouts/Header.vue';
+
+export default {
+  components: {
+    createAccount,
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+</style>

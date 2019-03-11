b941e51690dff5be70cce2038c58b6d3efdb8264
Kensei Yaegashi <s1F101702857@iniad.org>
2019-03-12 01:38:06 +0900

    svg追加 headermodal修正

--- components/header-modal.vue
+++ components/header-modal.vue
@@ -1,23 +1,27 @@
 <template lang="html">
   <div class="account_menu" id="account_menu">
     <ul>
+        <div class="header">
+          <p class="name">ジョージハリスン</p>
+          <p class="id">oswald_ya</p>
+        </div>
       <nuxt-link to="/create" class="sp">
-        <li><i class="fas fa-campground"></i>
+        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="rgb(163,167,161)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
           <p>New camp</p>
         </li>
       </nuxt-link>
       <nuxt-link to="/account">
-        <li><i class="far fa-user"></i>
+        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="rgb(163,167,161)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
           <p>マイページ</p>
         </li>
       </nuxt-link>
       <nuxt-link to="/create">
-        <li><i class="far fa-bookmark"></i>
+        <li><svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 24 24" fill="none" stroke="rgb(163,167,161)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
           <p>保存したキャンプ</p>
         </li>
       </nuxt-link>
       <a href="#">
-        <li><i class="fas fa-sign-out-alt"></i>
+        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="rgb(163,167,161)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4M10 17l5-5-5-5M13.8 12H3"/></svg>
           <p>ログアウト</p>
         </li>
       </a>
@@ -40,27 +44,54 @@ export default {
   font-size: 14px;
   position: absolute;
   top:75px;
-  right:170px;
+  right:128px;
+  &:before{
+    content: "";
+    position: absolute;
+    top: -26px;
+    left: 70%;
+    margin-left: -15px;
+    border: 13px solid transparent;
+    border-bottom: 13px solid #fff;
+  }
   ul {
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
     background-color: #fff;
     border-radius: 8px;
-    padding: 1rem 0;
+    padding: 0rem 0 1rem;
     font-weight: normal;
+    .header {
+      width: 100%;
+      padding: 1rem;
+      border-bottom: $border01;
+      p {
+        margin: 0;
+      }
+      .name{
+        font-size: 1rem;
+        font-weight: bold;
+        color: $black01;
+      }
+      .id {
+        font-size: 0.75rem;
+        color: $black02;
+      }
+    }
+
     li {
       font-size: 14px;
       padding: 6px 12px;
       display: flex;
       align-items: center;
+      margin: 0.3rem 0;
       p {
-
+        margin-left: 0.5rem;
       }
       i {
         color: $sub_color;
       }
       &:hover {
         background-color: #eee;
-        transition: $transtion01;
       }
     }
   }
@@ -79,20 +110,47 @@ export default {
     font-size: 14px;
     position: absolute;
     top:75px;
-    right:170px;
+    right:130px;
+    &:before{
+      content: "";
+      position: absolute;
+      top: -26px;
+      left: 70%;
+      margin-left: -15px;
+      border: 13px solid transparent;
+      border-bottom: 13px solid #fff;
+    }
     ul {
       box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
       background-color: #fff;
       border-radius: 8px;
-      padding: 1rem 0;
+      padding: 0 0 1rem;
       font-weight: normal;
+      .header {
+        width: 100%;
+        padding: 1rem;
+        border-bottom: $border01;
+        p {
+          margin: 0;
+        }
+        .name{
+          font-size: 1rem;
+          font-weight: bold;
+          color: $black01;
+        }
+        .id {
+          font-size: 0.75rem;
+          color: $black02;
+        }
+      }
       li {
         font-size: 14px;
         padding: 6px 12px;
         display: flex;
         align-items: center;
+        margin: 0.3rem 0;
         p {
-          margin-left: 1rem;
+          margin-left: 0.5rem;
         }
         i {
           color: $sub_color;
@@ -114,20 +172,47 @@ export default {
     font-size: 14px;
     position: absolute;
     top:75px;
-    right:40px;
+    right:20px;
+    &:before{
+      content: "";
+      position: absolute;
+      top: -26px;
+      left: 80%;
+      margin-left: -15px;
+      border: 13px solid transparent;
+      border-bottom: 13px solid #fff;
+    }
     ul {
       box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
       background-color: #fff;
       border-radius: 8px;
-      padding: 1rem 0;
+      padding: 0rem 0 1rem;
       font-weight: normal;
+      .header {
+        width: 100%;
+        padding: 1rem;
+        border-bottom: $border01;
+        p {
+          margin: 0;
+        }
+        .name{
+          font-size: 1rem;
+          font-weight: bold;
+          color: $black01;
+        }
+        .id {
+          font-size: 0.75rem;
+          color: $black02;
+        }
+      }
       li {
         font-size: 14px;
         padding: 6px 12px;
         display: flex;
         align-items: center;
+        margin: 0.3rem 0;
         p {
-          margin-left: 1rem;
+          margin-left: 0.5rem;
         }
         i {
           color: $sub_color;
--- layouts/Header.vue
+++ layouts/Header.vue
@@ -16,7 +16,7 @@
         </div>
         <nuxt-link to="/create">
           <div class="submit btn">
-            <i class="fas fa-plus"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#00a496" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
             <p>New Camp</p>
           </div>
         </nuxt-link>

f3249aca51b298779231b0eae8a32dfddbdfe2b1
Kensei Yaegashi <s1F101702857@iniad.org>
2019-04-03 22:33:56 +0900

    'modify_phrase'

--- components/camp-body/joinbtn.vue
+++ components/camp-body/joinbtn.vue
@@ -7,11 +7,11 @@
 
     <div class="btn_priority" @click="doJoin()" v-if="!classJoin">
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-     <p>Attend Camp</p>
+     <p>Campに参加</p>
     </div>
     <div class="btn_priority joined" @click="doUnjoin()" v-if="classJoin">
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-     <p>Attended Camp</p>
+     <p>参加中</p>
     </div>
   </div>
 </template>
